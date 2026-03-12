cf2db84bb0d34d583dc9d2184007e2db3667fe7c
Care32-Dental <care32dentalclinicbkt@gmail.com>
2026-03-12 17:37:31 +0545

    update emailjs credentials

--- api/sendEmail.ts
+++ api/sendEmail.ts
@@ -1,8 +1,8 @@
 import emailjs from "@emailjs/browser";
 
-const SERVICE_ID = "service_7t4vxwo";
-const TEMPLATE_ID = "template_4ohw4rc";
-const PUBLIC_KEY = "B8pXuY23f--MVUCmE";
+const SERVICE_ID = "service_sxvvs3a";
+const TEMPLATE_ID = "template_xpdx5hu";
+const PUBLIC_KEY = "gj2e-L8xwDVMePXsr";
 
 export const sendEmail = async (content: {
   fullName: string;
